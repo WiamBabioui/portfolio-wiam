0aaee50abf4f6880dbc116d51c23d54365c9b639
Wiam_BABIOUI <wiam.babioui@teknologiate.com>
2026-06-04 15:25:49 +0100

    Fix: Set build output directory to build for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Permet d'utiliser JSX dans les fichiers .js (comme index.js, App.js)
+  
+  // AJOUTEZ CE BLOC ICI pour corriger l'erreur Vercel :
+  build: {
+    outDir: 'build', // Force Vite à créer un dossier 'build' au lieu de 'dist'
+  },
+
+  // On garde vos configurations actuelles pour le JSX
   esbuild: {
     loader: 'jsx',
     include: /src\/.*\.[jt]sx?$/,
